2dde9601ab5693c890a07bd6b8535cc0aa6f7426
fuchun guo <117064092@qq.com>
2013-03-17 13:23:22 +0800

    Init rails project with mysql database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms6bey::Application.config.secret_token = '9c833ee7964791233a5bfd955fd13b6c7ea0503a3833c8f1976dd1c6c87f8703e3b48101588fba3df7df9230c81edd0cac72d42b6eab93827dbbfda2b760676b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cms6bey::Application.config.session_store :cookie_store, key: '_cms_6bey_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cms6bey::Application.config.session_store :active_record_store
