989ee68f28d089e5e2ace9269af46a5ec565b3b4
Ailenswpu <renkaiswpu@gmail.com>
2013-03-30 21:57:47 +0800

    deal select tag

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,6 +2,7 @@
 class Deal < ActiveRecord::Base
   attr_accessible :image_path, :price, :real_path, :title
   belongs_to :tag
+  attr_accessible :tag_id
   rails_admin do
     label_plural '商品管理'
   end
