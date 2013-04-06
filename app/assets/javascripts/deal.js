1a710bbd1e9336e460ff99f379f3d710758ba57c
Ken Lu <ken@seeice.com>
2013-04-05 22:34:31 -0400

    edited the tooltip fixed the location problem

--- app/assets/javascripts/deal.js
+++ app/assets/javascripts/deal.js
@@ -35,17 +35,6 @@
 
 $(function() {
   $('.pic').tooltip({
-    position: {
-      my: "center bottom-20",
-      at: "center top",
-      using: function( position, feedback ) {
-        $( this ).css( position );
-        $( "<div>" )
-          .addClass( "arrow" )
-          .addClass( feedback.vertical )
-          .addClass( feedback.horizontal )
-          .appendTo( this );
-      }
-    }
+    track: true
   });
 });
